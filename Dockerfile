5b7ed37964329c22bb58b73b32b17a970a23340b
Daljeet Singh <daljeet.singh123@gmail.com>
2021-09-01 14:44:32 +0530

    modified:   Dockerfile 	modified:   src/main/java/com/demo/order/OrderController.java

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
-FROM maven:3.5.2-jdk-8-alpine AS MAVEN_BUILD
-MAINTAINER Karthik Gunasekaran
-COPY pom.xml /build/
-COPY src /build/src/
-WORKDIR /build/
-RUN mvn package
-
-FROM openjdk:8-jdk-alpine
+FROM openjdk:8-jre-alpine
 WORKDIR /app
-COPY --from=MAVEN_BUILD /build/target/*.jar /app/app.jar
+COPY target/*.jar /app/app.jar
 ENTRYPOINT ["java","-jar","app.jar"]
-EXPOSE 8082
+EXPOSE 8081
+
 
--- src/main/java/com/demo/order/OrderController.java
+++ src/main/java/com/demo/order/OrderController.java
@@ -37,18 +37,18 @@ public class OrderController {
 		
 		final String finalPlan = plan;
 
-        Mono<String> billq = Mono.just(finalPlan +"49 USD");
+        //Mono<String> billq = Mono.just(finalPlan +"49 USD");
 		
-		/*webClient.get().uri("/billq/{id}",id)
+		return webClient.get().uri("/billq/{id}",id)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .bodyToMono(String.class)
                 .map(bill -> finalPlan + bill)
-                .log("Customer billq: ");*/
+                .log("Customer billq: ");
 
 
 
-        return billq;
+        //return billq;
 
     }
 
